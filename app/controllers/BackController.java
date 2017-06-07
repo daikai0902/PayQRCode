4d8cf04f3c988d583dba9b4700d7b455ca0dacfb
daikai0902 <1040526401@qq.com>
2017-06-07 17:11:07 +0800

    222

--- app/controllers/BackController.java
+++ app/controllers/BackController.java
@@ -1,7 +1,16 @@
 package controllers;
 
+import cn.bran.play.JapidController;
+
 /**
  * Created by daikai on 2017/6/7.
  */
-public class BackController {
+public class BackController extends JapidController {
+
+
+    public static void index(){
+        renderJapid();
+    }
+
+
 }
